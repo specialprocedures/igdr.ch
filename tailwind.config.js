8b73a2ede0feb790db69325ee8196cf52afd91f5
Ian Goodrich <goodrichian@gmail.com>
2025-11-24 03:06:07 +0400

    theme: catpuccin

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,44 +9,57 @@ module.exports = {
       colors: {
         transparent: 'transparent',
         current: 'currentColor',
+        // Catppuccin Mocha color palette
+        'base': '#1e1e2e',
+        'mantle': '#181825',
+        'crust': '#11111b',
+        'surface0': '#313244',
+        'surface1': '#45475a',
+        'surface2': '#585b70',
+        'overlay0': '#6c7086',
+        'overlay1': '#7f849c',
+        'overlay2': '#9399b2',
+        'text': '#cdd6f4',
+        'subtext0': '#a6adc8',
+        'subtext1': '#bac2de',
         'primary': {
-          DEFAULT: '#6366F1',
-          50: '#FFFFFF',
-          100: '#F9F9FE',
-          200: '#D3D4FB',
-          300: '#AEAFF8',
-          400: '#888BF4',
-          500: '#6366F1',
-          600: '#3034EC',
-          700: '#1317D1',
-          800: '#0E119E',
-          900: '#0A0C6A'
+          DEFAULT: '#89b4fa', // Blue
+          50: '#cdd6f4',
+          100: '#bac2de',
+          200: '#a6adc8',
+          300: '#89b4fa',
+          400: '#74c7ec', // Sapphire
+          500: '#89b4fa',
+          600: '#89b4fa',
+          700: '#74c7ec',
+          800: '#89dceb', // Sky
+          900: '#94e2d5' // Teal
         },
         'secondary': {
-          DEFAULT: '#EC4899',
-          50: '#FDEEF6',
-          100: '#FBDCEB',
-          200: '#F8B7D7',
-          300: '#F492C2',
-          400: '#F06DAE',
-          500: '#EC4899',
-          600: '#E4187D',
-          700: '#B11261',
-          800: '#7F0D45',
-          900: '#4C0829'
+          DEFAULT: '#f5c2e7', // Pink
+          50: '#f5e0dc',
+          100: '#f2cdcd',
+          200: '#f5c2e7',
+          300: '#cba6f7', // Mauve
+          400: '#f5c2e7',
+          500: '#f5c2e7',
+          600: '#cba6f7',
+          700: '#cba6f7',
+          800: '#b4befe', // Lavender
+          900: '#cba6f7'
         },
         'neutral': {
-          DEFAULT: '#6B7280',
-          50: '#CDD0D5',
-          100: '#C2C5CC',
-          200: '#ACB0BA',
-          300: '#969BA7',
-          400: '#7F8694',
-          500: '#6B7280',
-          600: '#515761',
-          700: '#383C43',
-          800: '#1E2024',
-          900: '#050506'
+          DEFAULT: '#6c7086',
+          50: '#cdd6f4',
+          100: '#bac2de',
+          200: '#a6adc8',
+          300: '#9399b2',
+          400: '#7f849c',
+          500: '#6c7086',
+          600: '#585b70',
+          700: '#45475a',
+          800: '#313244',
+          900: '#1e1e2e'
         },
         // To change these, use https://www.tailwindshades.com/ with https://tailwindcss.com/docs/customizing-colors or create your own custom colors.
       },
@@ -57,38 +70,38 @@ module.exports = {
       typography: (theme) => ({
         DEFAULT: {
           css: {
-            '--tw-prose-body': theme('colors.zinc[800]'),
-            '--tw-prose-headings': theme('colors.zinc[900]'),
-            '--tw-prose-lead': theme('colors.zinc[700]'),
-            '--tw-prose-links': theme('colors.zinc[900]'),
-            '--tw-prose-bold': theme('colors.zinc[900]'),
-            '--tw-prose-counters': theme('colors.zinc[600]'),
-            '--tw-prose-bullets': theme('colors.zinc[400]'),
-            '--tw-prose-hr': theme('colors.zinc[300]'),
-            '--tw-prose-quotes': theme('colors.zinc[900]'),
-            '--tw-prose-quote-borders': theme('colors.zinc[300]'),
-            '--tw-prose-captions': theme('colors.zinc[700]'),
-            '--tw-prose-code': theme('colors.indigo[500]'),
-            '--tw-prose-pre-code': theme('colors.indigo[300]'),
-            '--tw-prose-pre-bg': theme('colors.gray[900]'),
-            '--tw-prose-th-borders': theme('colors.zinc[300]'),
-            '--tw-prose-td-borders': theme('colors.zinc[200]'),
-            '--tw-prose-invert-body': theme('colors.zinc[200]'),
-            '--tw-prose-invert-headings': theme('colors.white'),
-            '--tw-prose-invert-lead': theme('colors.zinc[300]'),
-            '--tw-prose-invert-links': theme('colors.indigo[400]'),
-            '--tw-prose-invert-bold': theme('colors.white'),
-            '--tw-prose-invert-counters': theme('colors.zinc[400]'),
-            '--tw-prose-invert-bullets': theme('colors.zinc[200]'),
-            '--tw-prose-invert-hr': theme('colors.zinc[500]'),
-            '--tw-prose-invert-quotes': theme('colors.zinc[100]'),
-            '--tw-prose-invert-quote-borders': theme('colors.zinc[700]'),
-            '--tw-prose-invert-captions': theme('colors.zinc[400]'),
-            '--tw-prose-invert-code': theme('colors.indigo[400]'),
-            '--tw-prose-invert-pre-code': theme('colors.indigo[300]'),
-            '--tw-prose-invert-pre-bg': theme('colors.gray[900]'),
-            '--tw-prose-invert-th-borders': theme('colors.zinc[100]'),
-            '--tw-prose-invert-td-borders': theme('colors.zinc[500]'),
+            '--tw-prose-body': '#cdd6f4', // text
+            '--tw-prose-headings': '#cdd6f4', // text
+            '--tw-prose-lead': '#bac2de', // subtext1
+            '--tw-prose-links': '#89b4fa', // blue
+            '--tw-prose-bold': '#cdd6f4', // text
+            '--tw-prose-counters': '#a6adc8', // subtext0
+            '--tw-prose-bullets': '#9399b2', // overlay2
+            '--tw-prose-hr': '#585b70', // surface2
+            '--tw-prose-quotes': '#bac2de', // subtext1
+            '--tw-prose-quote-borders': '#585b70', // surface2
+            '--tw-prose-captions': '#a6adc8', // subtext0
+            '--tw-prose-code': '#f5c2e7', // pink
+            '--tw-prose-pre-code': '#cdd6f4', // text
+            '--tw-prose-pre-bg': '#181825', // mantle
+            '--tw-prose-th-borders': '#585b70', // surface2
+            '--tw-prose-td-borders': '#45475a', // surface1
+            '--tw-prose-invert-body': '#cdd6f4', // text
+            '--tw-prose-invert-headings': '#cdd6f4', // text
+            '--tw-prose-invert-lead': '#bac2de', // subtext1
+            '--tw-prose-invert-links': '#89b4fa', // blue
+            '--tw-prose-invert-bold': '#cdd6f4', // text
+            '--tw-prose-invert-counters': '#a6adc8', // subtext0
+            '--tw-prose-invert-bullets': '#9399b2', // overlay2
+            '--tw-prose-invert-hr': '#585b70', // surface2
+            '--tw-prose-invert-quotes': '#bac2de', // subtext1
+            '--tw-prose-invert-quote-borders': '#45475a', // surface1
+            '--tw-prose-invert-captions': '#a6adc8', // subtext0
+            '--tw-prose-invert-code': '#f5c2e7', // pink
+            '--tw-prose-invert-pre-code': '#cdd6f4', // text
+            '--tw-prose-invert-pre-bg': '#181825', // mantle
+            '--tw-prose-invert-th-borders': '#585b70', // surface2
+            '--tw-prose-invert-td-borders': '#45475a', // surface1
           },
         },
       }),
